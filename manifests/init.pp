453551ce379ca07026fa160475b6e9aa76fd4a3a
Samuel Leathers <sam@appliedtrust.com>
2014-03-07 15:51:28 -0500

    simplifying module further

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,15 +6,9 @@ class wpcli (
 
 ) {
 
-  exec { 'fetch-wpcli':
-    cwd     => '/tmp',
-    command => 'curl -L https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar > wp-cli.phar',
-    creates => '/tmp/wp-cli.phar',
-  }
-
   file { $destination:
-    ensure  => file,
-    source  => '/tmp/wp-cli.phar',
-    require => Exec['fetch-wpcli'],
+    ensure => file,
+    source => 'https://raw.github.com/wp-cli/builds/gh-pages/phar/wp-cli.phar',
+    mode   => '0755',
   }
 }
